6a0987794383bb06494eada11dda70c1b11e9e89
Iuri de Lima Marques <iurilmarques@hotmail.com>
2021-10-12 17:55:54 -0300

    mergesort

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] unsortedArray = {5, 2, 4, 3, 1, 9, 15, 11};
+        mergeSort(unsortedArray, new int[unsortedArray.length], 0,unsortedArray.length - 1);
+        System.out.println(Arrays.toString(unsortedArray));
+    }
+
+    private static void mergeSort(int[] array, int[] temp, int leftStart, int rightEnd) {
+        if (leftStart >= rightEnd) {
+            return;
+        }
+        int middle = (leftStart + rightEnd) / 2;
+        mergeSort(array, temp, leftStart, middle);
+        mergeSort(array, temp, middle + 1, rightEnd);
+        mergeHalves(array, temp, leftStart, rightEnd);
+    }
+
+    private static void mergeHalves(int[] array, int[] temp, int leftStart, int rightEnd) {
+        System.out.println(leftStart + " - " + rightEnd);
+        int leftEnd = (rightEnd + leftStart) / 2;
+        int rightStart = leftEnd + 1;
+        int size = rightEnd - leftStart + 1;
+
+        int left = leftStart;
+        int right = rightStart;
+        int index = leftStart;
+
+        while (left <= leftEnd && right <= rightEnd) {
+            if (array[left] <= array[right]) {
+                temp[index] = array[left];
+                left++;
+            } else {
+                temp[index] = array[right];
+                right++;
+            }
+            index++;
+        }
+
+        System.arraycopy(array, left, temp, index, leftEnd - left + 1);
+        System.arraycopy(array, right, temp, index, rightEnd - right + 1);
+        System.arraycopy(temp, leftStart, array, leftStart, size);
+    }
+
+
+}
